a1f87f31ff17d975e75fa2bf8a3af0ba62f79bce
Eliška Hrdá <eliskah@post.cz>
2025-06-05 20:25:48 +0200

    added ValidationUtils for name, phone, email and password. set getter and setter in CatOwner and added validation to AuthenticationService

--- Backend/src/main/java/model/CatOwner.java
+++ Backend/src/main/java/model/CatOwner.java
@@ -1,5 +1,15 @@
-package src.main.java.model;
+package model;
 
 public class CatOwner {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
 
+    // Gettery a settery
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getPhone() { return phone; }
 }
